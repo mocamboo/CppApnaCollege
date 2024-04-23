4979d7e928e1032f16a9830ce38af2b471bdfd86
Shubham Jangid <shubham0jangid@gmail.com>
2024-04-23 18:34:42 +0530

    Add .gitignore

--- src/Binary Search Tree/linkedList.cpp
+++ src/Binary Search Tree/linkedList.cpp
@@ -10,26 +10,34 @@ public:
     Node* next;
 
     // Default Constructor
-    Node() : data(0), next(nullptr) { cout << "Default constructor called." << endl; }
+    Node() : data(0), next(nullptr) { cout << "Default constructor triggered... " << endl; }
 
     // Parameterized Constructor
-    Node(int data) : data(data), next(nullptr) { cout << "Parameterized constructor called with data = " << data << endl; }
+    Node(int data) : data(data), next(nullptr) { cout << "Parameterized constructor triggered... with data = " << data << endl; }
 
     // Copy Constructor
-    Node(const Node& other) : data(other.data), next(other.next) { cout << "Copy constructor called." << endl; }
+    Node(const Node& other) : data(other.data), next(other.next) { cout << "Copy constructor triggered... " << endl; }
 
-    ~Node() { cout << "Destructor called for node with data = " << data << endl; }
+    ~Node() { cout << "Destructor triggered... for node with data = " << data << endl; }
 };
 
 int main() {
-    // Node b; // Stack Allocation [default cons]
+    /*** Stack Allocation ***/
+    // Constructor Calls - Default constructor called >> Destructor called for node with data = -1944335688
+    Node b;
 
-    // Difference b/w Node a = *new Node(4);  and  Node* head = new Node(4);
-    // Node a = *new Node(4); // >> Parameterised Con >> Copy Cons >> Destructur
-    // Cons
-    Node* head = new Node(4); // Case 2
+    /*** Dynamic (Heap) Allocation ***/
+    /**
+     Case 1
+     Constructor Calls - Parameterized constructor called with data = 4 >> Copy constructor called >> Destructor called for node with data = 4
+     */
+    Node a = *new Node(4);
 
-    //   delete head; // Clean up
+    /**
+     Case 2
+     Constructor Calls -  Parameterized constructor called with data = 4
+     */
+    Node* head = new Node(9);
 
     return 0;
 }
